6c11ca197d50f519811f0b7d6f5468c551bf005d
Jean Morillo sanchez <morillo30@gmail.com>
2020-07-27 11:15:51 -0400

    adding routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>
+       <nav></nav>
+          <Switch>
+            {/* Main route */}
+            {/* exact means this is the first page to load */}
+            <Route exact path="/">
+         <h1>main route</h1>
+            </Route>
+            {/* Follow the same structure as above to show your page */}
+            <Route path="/signup">
+              {/* Replace H1 with component */}
+              <h1>Hi this is a route</h1>
+            </Route>
+            <Route path="/register">
+              <h1>Hi this is the officials route, place component here </h1>
+            </Route>
+
+            <Route path="/view">
+             
+            </Route>
+
+            <Route path="*" exact={true}  ><h1>helloo</h1></Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
