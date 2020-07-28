b28fb921ddec6be50934c78074be0225f8f821cc
alanzino11 <annalanzino@ufl.edu>
2020-07-28 16:11:03 -0400

    Fixed CSS for Protest View, added ability to get user location and used Google Maps API to get city from coordinates.

--- frontend/src/pages/protests/Protests.js
+++ frontend/src/pages/protests/Protests.js
@@ -1,5 +1,17 @@
 import React, { useEffect, useState } from "react"
 import axios from 'axios';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    Text,
+    Button,
+    useDisclosure,
+    Icon
+  } from "@chakra-ui/core";
 
 import SimpleMap from '../../components/simpleMap'
 import ProtestCard from '../../components/ProtestCard'
@@ -39,10 +51,13 @@ const defaultMarker = [{
 }];
 
 const Protests = () => {
+    const [protestClicked, setProtestClicked] = useState(defaultProtest[0])
+
     useEffect(() => {
         axios.get('http://localhost:3000/api/protests/')
         .then(res => {
             setProtests(res.data)
+            setProtestClicked(res.data[0])
             let tempMarkers = [];
             for (const m of res.data) {
                 tempMarkers.push({
@@ -52,12 +67,40 @@ const Protests = () => {
                 })
             }
             setMarkers(tempMarkers)
+        }).then(() => {
+
         })
     }, [])
 
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            console.log("Latitude is :", position.coords.latitude);
+            console.log("Longitude is :", position.coords.longitude);
+          });
+
+          axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${protestClicked.startLocation[0].location.latitude},${protestClicked.startLocation[0].location.longitude}&sensor=true&key=${process.env.REACT_APP_API_KEY}`)
+          .then(res => {
+              console.log('actual city: ', res.data.plus_code.compound_code)
+              let words = res.data.plus_code.compound_code.split(',')
+              let subWords = words[0].split(' ')
+              let city = '';
+              for (let i = 0; i < subWords.length; i++ ) {
+                if (i !== 0) {
+                    city += subWords[i]
+
+                    if (i !== subWords.length - 1)
+                    city += ' '
+                }
+              }
+              console.log('CITY: ', city)
+          })
+    }, [protestClicked])
+
     const [protests, setProtests] = useState(defaultProtest)
-    const [protestClicked, setProtestClicked] = useState(defaultProtest[0])
     const [markers, setMarkers] = useState(defaultMarker)
+    const [isOpen, setIsOpen] = useState(false)
+    const [protestClickedTime, setProtestClickedTime] = useState('10:00')
+    const { onClose } = useDisclosure();
 
     const search = (id) => {
         for (var i = 0; i < protests.length; i++) {
@@ -71,14 +114,59 @@ const Protests = () => {
         setProtestClicked(search(id))
     }
 
+    const openModal = (time) => {
+        setProtestClickedTime(time)
+        setIsOpen(true)
+    }
+
+    const getResources = () => {
+        let temp = '';
+
+        protestClicked.resources.map((r, index) => {
+            temp += r
+            let c = index === protestClicked.resources.length - 1 ? '' : ', '
+            temp += c
+        })
+
+        return temp
+    }
+
     return (
         <div className="protests">
             <div className="row">
-                <div className="column"><SimpleMap clickedMarker={{lat: protestClicked.startLocation[0].location.latitude, lng: protestClicked.startLocation[0].location.longitude, title: protestClicked.title}} markers={markers}/></div>
-                <div className="column">
+                <div className="leftcolumn"><SimpleMap clickedMarker={{lat: protestClicked.startLocation[0].location.latitude, lng: protestClicked.startLocation[0].location.longitude, title: protestClicked.title}} markers={markers}/></div>
+                <div className="rightcolumn">
                 {protests.map(p => 
-                    <div><ProtestCard protest={p} isClicked={protestClicked._id === p._id ? true : false} onCardClick={onCardClick}/></div>
+                    <div>
+                        <ProtestCard protest={p} isClicked={protestClicked._id === p._id ? true : false} onCardClick={onCardClick} openModal={openModal}/>
+                    </div>
                 )}
+                <Modal isOpen={isOpen} onClose={onClose}>
+                    <ModalOverlay />
+                    <ModalContent>
+                    <ModalHeader>{protestClicked.title}</ModalHeader>
+                    <ModalBody>
+                        Start Location: {protestClicked.startLocation[0].location.city}
+                    </ModalBody>
+                    <ModalBody>
+                        End Location: {protestClicked.endLocation[0].location.city}
+                    </ModalBody>
+                    <ModalBody>
+                        <h1>Time: {protestClickedTime}</h1>
+                    </ModalBody>
+                    <ModalBody>
+                        <h2>{protestClicked.summary}</h2>
+                    </ModalBody>
+                    <ModalBody>
+                        <h2>Resources needed: {getResources()}</h2>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button variantColor="blue" mr={3} onClick={() => setIsOpen(false)}>
+                        Close
+                        </Button>
+                    </ModalFooter>
+                    </ModalContent>
+                </Modal>
                 </div>
             </div>
         </div>
