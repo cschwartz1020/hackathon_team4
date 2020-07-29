6c58a39970940eae72e9b95aa1cb05e12efea71d
Jean Morillo sanchez <morillo30@gmail.com>
2020-07-29 14:47:31 -0400

    adding working covid location markers by lat and lng

---
+++ frontend/src/components/marker2.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import "../css/marker2.css"
+
+const Marker = (props) => {
+    const { color, name ,confirmed} = props;
+
+function covidColordetector(confirmedNumber) {
+  if(confirmedNumber <100){
+     return "green";
+  }else {
+    if(confirmedNumber <300)
+    {
+      return "yellow";
+    }else{
+      return "red";
+    }
+  }
+
+}
+
+    return (
+      <React.Fragment>
+        <div className="marker-container">
+      <div className="marker"
+        style={{ backgroundColor: covidColordetector(confirmed), cursor:"pointer" }}
+        title={name}
+      />
+      <div className="tool-tip">
+        <h3 style={{margin:"5px 0 3px",fontWeight:"600"}}> {name}</h3>
+        <h3>Confirmed cases: </h3>
+        <span style={{color:"rgb(0, 183, 255)",fontWeight:"600"}}>{confirmed}</span>
+      </div>
+      </div>
+      </React.Fragment>
+    );
+  };
+
+  export default Marker;
--- frontend/src/components/simpleMap2.js
+++ frontend/src/components/simpleMap2.js
@@ -1,86 +1,144 @@
 import React, { useEffect, useState,useCallback } from "react"
 import GoogleMapReact from 'google-map-react';
-import Marker from "./marker"
-
-
+import Marker2 from "./marker2"
+import "../css/marker2.css"
+import axios from "axios";
 
 
 const SimpleMap2 = ({ markers, clickedMarker, zoom }) => {
-  const [bounds, setBounds] = useState({value:{northEast:{lat:34,lng:34},southWest:{lat:34,lng:34}}})
-  // const [bounds, setBounds] = useState({value:'initial value'})
-  let center = {lat: clickedMarker.lat, lng: clickedMarker.lng}
-
-  const setValue = useCallback((newValue) => {
-    setBounds({ ...bounds, value: newValue })
-
-  }, [bounds]) 
-
-  useEffect(() => {
-    console.log("checkbounds: ",bounds); 
-  },[bounds]);
-
-  const apiIsLoaded = (map, maps) => {
-    // Get bounds by our places
-    // Fit map to bounds
-     var CornerBounds = map.getBounds();
-
-     setValue({northEast:{lat:CornerBounds.getNorthEast().lat(),lng:CornerBounds.getNorthEast().lng()},southWest:{lat:CornerBounds.getSouthWest().lat(),lng:CornerBounds.getSouthWest().lng()}})
-
-    //  console.log(bounds1.getNorthEast().lat() ,bounds1.getNorthEast().lng() )
-    //  console.log(bounds1.getSouthWest().lat() ,bounds1.getSouthWest().lng() )
-  
-    map.addListener('dragend', function() {
-      var CornerBounds = map.getBounds();
-
-      // set northEast and SouthWest coordinates
-      setValue({northEast:{lat:CornerBounds.getNorthEast().lat(),lng:CornerBounds.getNorthEast().lng()},southWest:{lat:CornerBounds.getSouthWest().lat(),lng:CornerBounds.getSouthWest().lng()}})
-    });
-
+    const defaultZoomLevel = 6;
+    const [bounds, setBounds] = useState({value:{northEast:{lat:34,lng:34},southWest:{lat:34,lng:34}}})
+    const [covidMarkers, setCovidMarkers] = useState({value:[{region:{cities:[{lat:34,lng:-121,name:"sddsd"}],province:""}}]})
+    const [isCovidDataLoaded,setIsisCovidDataLoaded] = useState(false);
+    const [mapZoom,setmapZoom] = useState(defaultZoomLevel);
    
+    
+    let center = {lat: clickedMarker.lat, lng: clickedMarker.lng}
 
-    map.addListener('zoom_changed', function() {
-      var CornerBounds = map.getBounds();
 
-      // set northEast and SouthWest coordinates
-      setValue({northEast:{lat:CornerBounds.getNorthEast().lat(),lng:CornerBounds.getNorthEast().lng()},southWest:{lat:CornerBounds.getSouthWest().lat(),lng:CornerBounds.getSouthWest().lng()}})
-    });
-  
-  };
-  
-  const defaultProps = {
-    center: {
-      lat: 28,
-      lng: 86
-    },
-    zoom: 11
-  };
+    // == Load Google Maps API ==
+    const apiIsLoaded = (map, maps) => {
+        //set zoom
+        var zoom = map.getZoom();
+    
+         // iterate over markers and call setVisible
+        if(zoom <= defaultZoomLevel){
+            setIsisCovidDataLoaded(false);
+            setmapZoom(false)
+        }else{
+            setIsisCovidDataLoaded(true);
+            setmapZoom(true)
+        }
 
-  return (
-    <div style={{ height: '100%', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{key: process.env.REACT_APP_API_KEY}}
-        defaultCenter={defaultProps.center}
-        center={center}
-        defaultZoom={defaultProps.zoom}
-        yesIWantToUseGoogleMapApiInternals
-        onGoogleApiLoaded={({ map, maps }) => apiIsLoaded(map, maps)}
-      >
-        {markers.map(m =>  // [{lat, lng, title}]
-          <Marker
-            lat={m.lat}
-            lng={m.lng}
-            text={m.title}
-          />
-        )}
+        // Fit map to bounds
+        var CornerBounds = map.getBounds();
+        setBoundsCallback({northEast:{lat:CornerBounds.getNorthEast().lat(),lng:CornerBounds.getNorthEast().lng()},southWest:{lat:CornerBounds.getSouthWest().lat(),lng:CornerBounds.getSouthWest().lng()}})
 
-          <Marker
-            lat={34.297766572244406}
-            lng={-118.7588887724533}
-            text={"sdsdsdsd"}
-          />
-      </GoogleMapReact>
-    </div>
-  );
+        // Add Drag end Listener to map
+        map.addListener('dragend', function() {
+            var CornerBounds = map.getBounds();
+            // set northEast and SouthWest coordinates
+            setBoundsCallback({northEast:{lat:CornerBounds.getNorthEast().lat(),lng:CornerBounds.getNorthEast().lng()},southWest:{lat:CornerBounds.getSouthWest().lat(),lng:CornerBounds.getSouthWest().lng()}})
+        });
+
+        // Add zoom changed Listener to map
+        map.addListener('zoom_changed', function() {
+
+            var zoom = map.getZoom();
+
+            // iterate over markers and call setVisible
+            if(zoom <= defaultZoomLevel){
+                setIsisCovidDataLoaded(false);
+                setmapZoom(false)
+            }else{
+                setIsisCovidDataLoaded(true);
+                setmapZoom(true)
+            }
+
+            //Get bounds
+            var CornerBounds = map.getBounds();
+            // set northEast and SouthWest coordinates
+            setBoundsCallback({northEast:{lat:CornerBounds.getNorthEast().lat(),lng:CornerBounds.getNorthEast().lng()},southWest:{lat:CornerBounds.getSouthWest().lat(),lng:CornerBounds.getSouthWest().lng()}})
+        });
+    };
+
+    // == Callbacks ==
+    const setBoundsCallback = useCallback((newValue) => {
+        setBounds({ ...bounds, value: newValue })
+    }, [bounds]) 
+
+    const setCovidMarkersCallback = useCallback((newValue) => {
+        setCovidMarkers({ ...covidMarkers, value: newValue })
+    }, [covidMarkers]) 
+
+    // == Use Effects ==
+
+    useEffect(() => {
+        axios.get( `http://localhost:3000/api/covid/area/${bounds.value.northEast.lat}/${bounds.value.northEast.lng}/${bounds.value.southWest.lat}/${bounds.value.southWest.lng}`)
+        .then(res => {
+            setCovidMarkersCallback(res.data)
+
+            if(mapZoom){
+                setIsisCovidDataLoaded(true);
+                
+            }else{
+                setIsisCovidDataLoaded(false);
+            }
+        })
+    },[bounds]);
+
+    useEffect(() => {
+        try{
+        // console.log("covidMarkers: ",covidMarkers.value[0].region.province); 
+        }catch (e){
+        
+    }
+    
+    },[covidMarkers]);
+
+    
+    
+    const defaultProps = {
+        center: {
+        lat: 28,
+        lng: 86
+        },
+        zoom: 11
+    };
+
+    return (
+        <div style={{ height: '100%', width: '100%' }}>
+        <GoogleMapReact
+            bootstrapURLKeys={{key: process.env.REACT_APP_API_KEY}}
+            defaultCenter={defaultProps.center}
+            center={center}
+            defaultZoom={defaultProps.zoom}
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={({ map, maps }) => apiIsLoaded(map, maps)}
+        >
+            {markers.map(m =>  // [{lat, lng, title}]
+            <Marker2
+                lat={m.lat}
+                lng={m.lng}
+                text={m.title}
+            />
+            )}
+        {isCovidDataLoaded?covidMarkers.value.map(covidMarker => covidMarker.region.cities.map(city=> (
+                <Marker2
+            lat={city.lat}
+            lng={city.long}
+            name={city.name}
+            confirmed={city.confirmed}
+            />))):  <Marker2
+            lat={34}
+            lng={-121}
+            text={"sdsdsd"}
+        />}
+            
+        </GoogleMapReact>
+
+        </div>
+    );
 }
 
 export default SimpleMap2;
