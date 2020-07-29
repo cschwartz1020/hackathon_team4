bd6828761029e8b56c6c85f77c9a57729d7bfd60
Charles Schwartz <chschwartz@redventures.com>
2020-07-29 08:13:32 -0600

    prevents data dump if collection exists

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,11 @@ const app = new express();
 app.use(express.json());
 const routes = require("./routes");
 var cors = require("cors");
-app.use(cors());
 covidHelper = require("./util/covid/createCollection");
+app.use(cors());
 app.use(routes);
 
 app.listen(process.env.PORT || 3000, () => {
-  covidHelper.createCollection();
+  covidHelper.createCollectionIfNotExists();
   console.log("Server started on port 3000");
 });
