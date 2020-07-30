7252f5a7e1ca0fe79a9562c60891aa20e54215fb
Charles Schwartz <chschwartz@redventures.com>
2020-07-30 07:37:23 -0600

    adds get/update user by email

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -15,7 +15,18 @@ async function find_one_user(req, res) {
 async function find_user_and_update(req, res) {
   await userServices.findByIdAndUpdate(req, res);
 }
+
+async function find_user_by_email(req, res) {
+  await userServices.findByEmail(req, res);
+}
+
+async function find_user_by_email_and_update(req, res) {
+  await userServices.findByEmailAndUpdate(req, res);
+}
+
 module.exports.create_user = create_user;
 module.exports.find_all_users = find_all_users;
 module.exports.find_one_user = find_one_user;
 module.exports.find_user_and_update = find_user_and_update;
+module.exports.find_user_by_email = find_user_by_email;
+module.exports.find_user_by_email_and_update = find_user_by_email_and_update;
